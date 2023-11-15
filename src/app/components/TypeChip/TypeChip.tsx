f17cef9d9b4b836f9eb06601ca90f88a57250b11
Calvin Lee <calvin.hanbee.lee@gmail.com>
2023-11-15 17:34:27 +1300

    Add TypeChip crossout parameter

--- src/app/components/TypeChip/TypeChip.tsx
+++ src/app/components/TypeChip/TypeChip.tsx
@@ -5,17 +5,35 @@ import { Chip } from "@mui/material";
 
 interface Props {
   type: PokeType;
+  isCrossedOut?: boolean;
 }
 
 export default function TypeChip(props: Props) {
   const type = props.type;
   const typeInfo = getPokeTypeInfo(type);
   const colour = typeInfo ? typeInfo.colour : "#68A090";
+  const isCrossedOut = props.isCrossedOut === null ? false : props.isCrossedOut;
+
+  const style = isCrossedOut
+    ? {
+        backgroundColor: "white",
+        color: colour,
+        borderColor: colour,
+      }
+    : {
+        backgroundColor: colour,
+        color: "white",
+      };
 
   return (
     <Chip
+      sx={
+        isCrossedOut
+          ? { textAlign: "center", textDecoration: "line-through" }
+          : { textAlign: "center" }
+      }
       label={capitalise(type)}
-      style={{ backgroundColor: colour, color: "white" }}
+      style={style}
     />
   );
 }

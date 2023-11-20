7101442548828ffe3f8e1635ff1c94260613dc71
Calvin Lee <calvin.hanbee.lee@gmail.com>
2023-11-20 15:43:21 +1300

    PartyType uses storage

--- src/app/components/PartyTypesCard/PartyTypesListItem.tsx
+++ src/app/components/PartyTypesCard/PartyTypesListItem.tsx
@@ -4,6 +4,7 @@ import { ListItem, ListItemText, Switch } from "@mui/material";
 import { useState } from "react";
 import { PokeType } from "@/app/data/TypeDataInterface";
 import TypeChip from "../TypeChip/TypeChip";
+import { getFromStorage, saveToStorage } from "@/app/storage/browserStorage";
 
 interface Props {
   type: PokeType;
@@ -11,41 +12,77 @@ interface Props {
   bothPlayers: boolean;
 }
 
+interface PartyTypeStorage {
+  player1: boolean;
+  player2: boolean;
+}
+
 export default function PartyTypesListItem(props: Props) {
   const type = props.type;
   const bothPlayers = props.bothPlayers;
   const colour = props.colour;
-  const storageKey = "party-type-";
+  const storageKey = "party-type-" + props.type;
 
-  const getInitialValues = () => {};
+  const isStrikeoutCalculation = (playerOne: boolean, playerTwo: boolean) => {
+    if (playerOne || playerTwo) {
+      if (bothPlayers) {
+        if (playerOne && playerTwo) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  };
 
-  const [playerOneCheck, setPlayerOneCheck] = useState(false);
-  const [playerTwoCheck, setPlayerTwoCheck] = useState(false);
-  const [strikeout, setStrikeout] = useState(false);
+  const getInitialValues = () => {
+    const storedData = getFromStorage<PartyTypeStorage>(storageKey);
+
+    const player1Return = storedData === null ? false : storedData.player1;
+    const player2Return = storedData === null ? false : storedData.player2;
+    const strikeout = isStrikeoutCalculation(player1Return, player2Return);
+
+    return {
+      player1: player1Return,
+      player2: player2Return,
+      strikeOut: strikeout,
+    };
+  };
+
+  const initialValues = getInitialValues();
+
+  const [playerOneCheck, setPlayerOneCheck] = useState(initialValues.player1);
+  const [playerTwoCheck, setPlayerTwoCheck] = useState(initialValues.player2);
+  const [strikeout, setStrikeout] = useState(initialValues.strikeOut);
 
   const handleTogglePlayerOne = () => {
     const newValue = !playerOneCheck;
     setPlayerOneCheck(newValue);
     checkStrikeout(newValue, playerTwoCheck);
+    saveToStorage<PartyTypeStorage>(storageKey, {
+      player1: newValue,
+      player2: playerTwoCheck,
+    });
   };
 
   const handleTogglePlayerTwo = () => {
     const newValue = !playerTwoCheck;
     setPlayerTwoCheck(newValue);
     checkStrikeout(playerOneCheck, newValue);
+    saveToStorage<PartyTypeStorage>(storageKey, {
+      player1: playerOneCheck,
+      player2: newValue,
+    });
   };
 
   const checkStrikeout = (playerOne: boolean, playerTwo: boolean) => {
-    if (playerOne || playerTwo) {
-      if (bothPlayers) {
-        if (playerOne && playerTwo) {
-          setStrikeout(true);
-        } else {
-          setStrikeout(false);
-        }
-      } else {
-        setStrikeout(true);
-      }
+    const isStrikeout = isStrikeoutCalculation(playerOne, playerTwo);
+    if (isStrikeout) {
+      setStrikeout(true);
     } else {
       setStrikeout(false);
     }
